2e36db60960adb8c7405fff6d46c8f6aa2de50d5
AngelicaRoot <1423634265@qq.com>
2021-01-15 15:25:24 +0800

    Added PluginMetaData

---
+++ RollingScoreboard.py
@@ -0,0 +1,11 @@
+PLUGIN_METADATA = {
+    'id': 'rollingScoreboard',
+    'version': '1.1.1-Alpha1',
+    'name': 'RollingScoreboard',
+    'description': 'Highly customized rolling scoreboard plugin based on MCDR',
+    'author': 'WhitePaper',
+    'link': 'https://github.com/AngelicaRoot/MCDR-RollingScoreboard',
+    'dependencies': {
+        'mcdreforged': '>=1.0.2'
+    }
+}
