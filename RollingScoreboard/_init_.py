9814ae28dede6ff7000c27aa165b447888becc3d
AngelicaRoot <1423634265@qq.com>
2021-01-15 15:44:11 +0800

    Created Repo

---
+++ RollingScoreboard/_init_.py
@@ -0,0 +1,11 @@
+"""
+RollingScoreboard Plugin provides a large number of operations on the game scoreboard,including:
+    1.Add automatic display scoreboard
+    2.Custom scoreboard title (including title content, color and font style), statistical items, etc
+    3.Filtering statistical items with WhitePaper's whitelist plugin or the build-in automatic filter
+    4.Automatic start & stop rolling
+    5.Automatic clean server log file
+    etc
+This project is open source on GitHub and Gitee, and complies with gpl-3.0 protocol. I accept all constructive PRs and
+issues.
+"""
